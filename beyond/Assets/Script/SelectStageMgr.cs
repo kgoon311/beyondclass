1230f19bc39b7c418aa784c83db8a40a121f561f
sungbum05 <sblim05@naver.com>
2022-12-12 23:31:07 +0900

    SecenSelect

--- beyond/Assets/Script/SelectStageMgr.cs
+++ beyond/Assets/Script/SelectStageMgr.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using DG.Tweening;
-using UnityEditor;
 
 [System.Serializable]
 public class UnLockObj
@@ -17,8 +16,8 @@ public class SelectStageMgr : MonoBehaviour
 {
     public List<UnLockObj> UnLockObjs; 
 
-    public List<SceneAsset> DateScene;
-    public List<SceneAsset> CompanyScene;
+    public List<int> DateScene;
+    public List<int> CompanyScene;
 
     // Start is called before the first frame update
     void Start()
@@ -39,12 +38,12 @@ public class SelectStageMgr : MonoBehaviour
     public void GoDateGame()
     {
         SoundManager.In.PlaySoundClip("Click", ESoundType.SFX);
-        SceneManager.LoadScene(DateScene[Random.Range(0, DateScene.Count)].name);
+        SceneManager.LoadScene(DateScene[Random.Range(0, DateScene.Count)]);
     }
 
     public void GoCompanyGame()
     {
         SoundManager.In.PlaySoundClip("Click", ESoundType.SFX);
-        SceneManager.LoadScene(CompanyScene[Random.Range(0, CompanyScene.Count)].name);
+        SceneManager.LoadScene(CompanyScene[Random.Range(0, CompanyScene.Count)]);
     }
 }
