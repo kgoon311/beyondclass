890b190f33f4f98541b9b6a75cc15cfeaf98bba2
sungbum05 <sblim05@naver.com>
2022-12-13 01:06:28 +0900

    Molu

--- beyond/Assets/Script/GameManager.cs
+++ beyond/Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : SingletonMono<GameManager>
 {
@@ -9,6 +10,6 @@ public class GameManager : SingletonMono<GameManager>
     private void Start()
     {
         SoundManager.In.PlaySoundClip("BGM", ESoundType.BGM, 1, 1);
-
+        Screen.SetResolution(600, 1233, true);
     }
 }
